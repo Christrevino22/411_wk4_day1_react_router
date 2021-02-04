28d2567fe35291e0484e5a68a2ce011adbb39bdc
Christopher Trevino <chrisdadef@gmail.com>
2021-02-03 20:24:43 -0600

    made some small changes invloving the params as well as the car ID's

--- src/components/Car.js
+++ src/components/Car.js
@@ -3,16 +3,18 @@ import cars from '../cars.json'
 // import material ui components here //
 import {Container, Paper, Chip} from '@material-ui/core';
 // Container, Paper, Chip //
-
+//BELOW I BEILEVE I NEED TO FULLY FILL OUT THE FORM INVOLVED WITH THE FROM OR ATLEAST LOOK INTO FORMS!!!!!
 const Car = (props) => {
-    let id = props.match.params.id
+    let carId = props.match.params.id-1
+    const car = cars[carId]
+    console.log("this is the id bitch!!!!", car)
     return (
         <Container maxWidth="sm" className="car-container">
         <Paper className="car-paper">
             <h2>{cars.Name}</h2>
             {
                 Object.keys(cars).map((key, idx) => {
-                    return <Chip label={`${id}: ${cars[key]}`}></Chip>
+                    return <Chip label={`${key}: ${car[key]}`}></Chip>
                 })
             }
         </Paper>
